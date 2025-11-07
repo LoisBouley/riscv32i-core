d3ce2025bdfb76df7903bf23caefd73dfebbf90a
Marwa El Khazrouni <marwa.el-khazrouni@grenoble-inp.org>
2025-11-07 16:16:20 +0000

    correction orthographique

--- src/main/scala/projet/Rv32i.scala
+++ src/main/scala/projet/Rv32i.scala
@@ -9,6 +9,6 @@ class Rv32i(sim: Boolean = true) extends Module {
     val x31 = Output(UInt(32.W))
     val valid_x31  = if (sim)  Some(Output(Bool())) else None
   })
- // test pour modif sur gitlb
+ // test pour modif sur gitlab (avec un a marwa stp)
   locally(sim)
 }
