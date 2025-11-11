8580fdbe112923830ee79919493ff0f0182828eb
Marwa El Khazrouni <elkhazrouni.marwa@gmail.com>
2025-11-12 00:14:47 +0100

    Suite Etape 2

--- src/main/scala/projet/Rv32i.scala
+++ src/main/scala/projet/Rv32i.scala
@@ -2,6 +2,31 @@ package projet
 
 import chisel3._
 
+class RF(sim: Boolean = false) extends Module {
+  val io = IO(new Bundle {
+  val rsa_addr = Input(UInt(5.W))
+  val rsb_addr = Input(UInt(5.W))
+  val rw_addr = Input(UInt(5.W))
+  val rw_data = Input(UInt(32.W))
+  val we = Input(Bool())
+  val rsa_data = Output(UInt(32.W))
+  val rsb_data = Output(UInt(32.W))
+  })
+
+  // Banc de registres 
+  val banc_reg = Reg(Vec(32, UInt(32.W)))
+
+  io.rsa_data := Mux(io.rsa_addr === 0.U, 0.U, banc_reg(io.rsa_addr)) 
+  io.rsb_data := Mux(io.rsb_addr === 0.U, 0.U, banc_reg(io.rsb_addr))
+  when (io.we && io.rw_addr =/= 0.U) {
+    banc_reg(io.rw_addr) := io.rw_data
+  }
+
+  val imm = Cat(insn(31, 12), 0.U(12.W)) 
+    
+}
+
+
 class Rv32i(sim: Boolean = true) extends Module {
   val io = IO(new Bundle {
     val ibus = Flipped(new BusInterface)
@@ -9,7 +34,8 @@ class Rv32i(sim: Boolean = true) extends Module {
     val x31 = Output(UInt(32.W))
     val valid_x31  = if (sim)  Some(Output(Bool())) else None
   })
- io.ibus.wdata := 0.U
+
+  io.ibus.wdata := 0.U
 
   io.dbus.addr := 0.U
   io.dbus.wdata := 0.U
@@ -30,30 +56,12 @@ class Rv32i(sim: Boolean = true) extends Module {
 
   // ########################
 
-  class RF(sim: Boolean = false) extends Module {
-    val io = IO(new Bundle {
-      val rsa_addr = Input(UInt(5.W))
-      val rsb_addr = Input(UInt(5.W))
-      val rw_addr = Input(UInt(5.W))
-      val rw_data = Input(UInt(32.W))
-      val we = Input(Bool())
-      val rsa_data = Output(UInt(32.W))
-      val rsb_data = Output(UInt(32.W))
-    })
+  // Decodage de l'instruction
+  io.rsb_addr := insn(24, 20)
+  io.rsa_addr := insn(19, 15) 
+  io.rw_addr := insn(11, 7)
 
-    val banc_reg = Reg(Vec(32, UInt(32.W)))
-    io.rsa_data := Mux(io.rsa_addr === 0.U, 0.U, regs(io.rsa_addr)) 
-    io.rsb_data := Mux(io.rsb_addr === 0.U, 0.U, regs(io.rsb_addr))
-    when (io.we && io.rw_addr =/= 0.U) {
-        regs(io.rw_addr) := io.rw_data
-    }
-
-    io.rw_data := insn
-
-    val rf = 
-  }
-
-
+  dontTouch(x31)
   
   // locally(sim)
 }
