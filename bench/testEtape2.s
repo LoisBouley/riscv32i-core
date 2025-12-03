29a9fe263f083f200c4d345394a1c21a88f21839
Loïs BOULEY <lois.bouley@grenoble-inp.org>
2025-12-03 23:55:47 +0100

    Correctif test étape 2

--- bench/testEtape2.s
+++ bench/testEtape2.s
@@ -1,9 +1,12 @@
 #On ne peut pas faire d'auto test car on ne peut pas encore écrire dans le registre 31
 #pour tester l'étape 2, on regarde dans GTKWave que ImmU est bien construit et que RA, RB et RW sont bien connectés et décodés
 
-# expected: 00000000,00123000,00abc000
+# expected: fffff000,00123000,00abc000
 
     .text
+
+    nop
+    
     #ici on vérifie qu'on a ImmU bien construit (imm << 12). On execute donc des instructions de type U.
     lui x31, 0xfffff
     lui x31, 0x123
